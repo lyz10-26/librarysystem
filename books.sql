dec38109621151a8f9005c53dd622671f6a8020a
lyz10-26 <101388237+lyz10-26@users.noreply.github.com>
2022-03-11 19:13:50 +0800

    Add files via upload

---
+++ books.sql
@@ -0,0 +1,41 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80016
+ Source Host           : localhost:3306
+ Source Schema         : demo
+
+ Target Server Type    : MySQL
+ Target Server Version : 80016
+ File Encoding         : 65001
+
+ Date: 11/03/2022 19:13:21
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for books
+-- ----------------------------
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE `books`  (
+  `bookID` int(10) NOT NULL AUTO_INCREMENT COMMENT '书id',
+  `bookName` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '书名',
+  `bookCounts` int(11) NOT NULL COMMENT '数量',
+  `detail` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '描述',
+  INDEX `bookID`(`bookID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of books
+-- ----------------------------
+INSERT INTO `books` VALUES (1, 'Java', 13, '从入门到放弃');
+INSERT INTO `books` VALUES (2, 'MySQL', 10, '从删库到跑路');
+INSERT INTO `books` VALUES (3, 'Linux', 5, '从进门到进牢');
+INSERT INTO `books` VALUES (7, '盗墓笔记', 34, '快跑有这僵尸有毒');
+INSERT INTO `books` VALUES (8, '西游记', 13, '野猴子打小野怪');
+
+SET FOREIGN_KEY_CHECKS = 1;
